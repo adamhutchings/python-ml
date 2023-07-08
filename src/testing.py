05ff2514083f8325622f3e6890024dcb42883fda
adamhutchings <adam.abahot@gmail.com>
2023-07-08 19:46:29 -0400

    Make testing better (running fewer iterations)

--- src/testing.py
+++ src/testing.py
@@ -12,25 +12,24 @@ data.l = 138
 data.inputs = [x[:-1] for x in iris_data]
 data.outputs = [[x[-1]] for x in iris_data]
 
-net = nn.NeuralNetwork(1, 4, 1)
+nets = []
+for i in range(10):
+    nets.append(nn.NeuralNetwork(1, 4, 1))
 
-print('#################################################\n' * 3)
-# Iterate until the loss isn't improving considerably
-lowest_loss = 1_000_000_000
-i = 0
-try:
-    while True:
-        if i % 250 == 0:
-            loss = net.obtain_data_loss(data)
-            print(f'Iteration #{i} finished. Current loss: {loss}')
-        i += 1
+print('Training and selecting initial models ...')
+r = 500
+for i in range(r):
+    if i % (r / 10) == 0:
+        print(f'{round(i * 100 / r)}% done.')
+    for net in nets:
         net.learn(data, 0.001)
-except KeyboardInterrupt:
-    pass
 
-print(f'Algorithm halting after {i} iterations.')
+net = nn.NeuralNetwork(1, 4, 1)
+for n in nets:
+    if n.obtain_data_loss(data) < net.obtain_data_loss(data):
+        net = n
 
-print('Done training. Will test now ...')
+print(f'Obtained best net out of 10 with loss of {round(net.obtain_data_loss(data), 3)}.')
 
 # 20,000 iterations seems to do the job pretty well, except that inputs that
 # should be 0 are instead classed as -1. That's alright, though.
@@ -38,10 +37,14 @@ print('Done training. Will test now ...')
 # ten percent of the inputs.
 
 test_data = iread.read_test_data('iris/iris.data')
+inaccuracies = 0
 for datum in test_data:
     ins = datum[:-1]
     out = net.apply(ins)[0]
-    print('--------')
-    print(f'Data input: {ins}')
-    print(f'Expected output: {datum[-1]}')
-    print(f'Actual output: {out}')
+    # print('--------')
+    #print(f'Data input: {ins}')
+    #print(f'Expected output: {datum[-1]}')
+    #print(f'Actual output: {out}')
+    if round(datum[-1]) != round(out):
+        inaccuracies += 1
+print(f'In the model, {inaccuracies} out of {len(test_data)} were inaccurate.')
